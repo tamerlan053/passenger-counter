ad05b80d45ed193f9d61738635196ad1b52d2d75
tamerlan053 <97560103+tamerlan053@users.noreply.github.com>
2023-03-13 23:32:40 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 countEl = document.getElementById("count-el")
 countEl.textContent = 2;
 
-buttonEl = document.getElementById("buton-el")
+let count = 2;
 
+function increment() {
+    count += 1;
+    countEl.textContent = count;
+}
